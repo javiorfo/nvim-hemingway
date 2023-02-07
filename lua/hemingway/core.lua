17b8d9899d6f991f4b349b42183faf2cee756a17
Javier Orfo <javierorfo@protonmail.com>
2023-02-07 16:40:49 -0300

    comments table

--- lua/hemingway/core.lua
+++ lua/hemingway/core.lua
@@ -3,7 +3,7 @@
 -- # URL:          https://github.com/javio7/nvim-hemingway #
 -- ##########################################################
 
-local comment = require'hemingway'.comments[vim.bo.filetype]
+local comments = require'hemingway'.comments
 
 local M = {}
 local esc_code = vim.api.nvim_replace_termcodes("<esc>", true, true, true)
@@ -26,38 +26,38 @@ local function is_right_commented(text, comment_string)
 end
 
 local function get_final_single_line(text)
-    if not is_left_commented(text, comment.single) then
-        return comment.single .. text
+    if not is_left_commented(text, comments[vim.bo.filetype].single) then
+        return comments[vim.bo.filetype].single .. text
     end
-    return string.sub(text, #comment.single + 1)
+    return string.sub(text, #comments[vim.bo.filetype].single + 1)
 end
 
 local function get_final_left_line(text)
-    if not is_left_commented(text, comment.left) then
-        return comment.left .. text
+    if not is_left_commented(text, comments[vim.bo.filetype].left) then
+        return comments[vim.bo.filetype].left .. text
     end
-    return string.sub(text, #comment.left + 1)
+    return string.sub(text, #comments[vim.bo.filetype].left + 1)
 end
 
 local function get_final_right_line(text)
-    if not is_right_commented(text, comment.right) then
-        return text .. comment.right
+    if not is_right_commented(text, comments[vim.bo.filetype].right) then
+        return text .. comments[vim.bo.filetype].right
     end
-    return string.sub(text, 1, #text - #comment.right)
+    return string.sub(text, 1, #text - #comments[vim.bo.filetype].right)
 end
 
 function M.comment()
-    if comment then
+    if comments[vim.bo.filetype] then
         local line_number = vim.fn.line(".")
         local line_text = get_line(line_number)
 
-        if comment.single then
+        if comments[vim.bo.filetype].single then
             set_line(line_number, get_final_single_line(line_text))
         else
-            if not is_left_commented(line_text, comment.left) then
-                line_text = comment.left .. line_text .. comment.right
+            if not is_left_commented(line_text, comments[vim.bo.filetype].left) then
+                line_text = comments[vim.bo.filetype].left .. line_text .. comments[vim.bo.filetype].right
             else
-                line_text = string.sub(line_text, #comment.left + 1, #line_text - #comment.right)
+                line_text = string.sub(line_text, #comments[vim.bo.filetype].left + 1, #line_text - #comments[vim.bo.filetype].right)
             end
             set_line(line_number, line_text)
         end
@@ -65,7 +65,7 @@ function M.comment()
 end
 
 function M.multi_comment()
-    if comment then
+    if comments[vim.bo.filetype] then
         local start_line = vim.fn.line("v")
         local end_line = vim.fn.line(".")
 
@@ -75,7 +75,7 @@ function M.multi_comment()
             end_line = aux
         end
 
-        if comment.left then
+        if comments[vim.bo.filetype].left then
             local left_text = get_final_left_line(get_line(start_line))
             set_line(start_line, left_text)
 
