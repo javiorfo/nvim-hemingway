789dfa33b04c63349104da6243a2b7c72bd4f1ba
Javier Orfo <javierorfo@protonmail.com>
2023-03-28 00:34:12 -0300

    Update hemingway.lua

--- plugin/hemingway.lua
+++ plugin/hemingway.lua
@@ -1,7 +1,7 @@
--- ############################################################
--- # Maintainer: System Malt                                  #
--- # URL:        https://github.com/systemmalt/nvim-hemingway #
--- ############################################################
+-- ##########################################################
+-- # Maintainer: Javier Orfo                                #
+-- # URL:        https://github.com/javiorfo/nvim-hemingway #
+-- ##########################################################
 
 if vim.g.hemingway then
   return
