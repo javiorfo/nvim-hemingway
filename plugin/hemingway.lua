6ae81a7ce7f968abc51fa01fc83452a311e28cf2
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:29:54 -0300

    Update hemingway.lua

--- plugin/hemingway.lua
+++ plugin/hemingway.lua
@@ -1,8 +1,3 @@
--- ###########################################################
--- # Maintainer: Mr. Charkuils                               #
--- # URL:        https://github.com/charkuils/nvim-hemingway #
--- ###########################################################
-
 if vim.g.hemingway then
   return
 end
