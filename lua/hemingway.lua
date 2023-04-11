96f2ef1ff3cdcba9a3579344f53091652913c6b1
Javier Orfo <javierorfo@protonmail.com>
2023-04-11 12:29:08 -0300

    Update hemingway.lua

--- lua/hemingway.lua
+++ lua/hemingway.lua
@@ -1,8 +1,3 @@
--- ###########################################################
--- # Maintainer: Mr. Charkuils                               #
--- # URL:        https://github.com/charkuils/nvim-hemingway #
--- ###########################################################
-
 local util = require'hemingway.util'
 local Logger = util.Logger
 
